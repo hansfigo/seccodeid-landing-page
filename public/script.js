9c7cab0ac628df9e0dcb9da6649d861eaf9320d9
hansfigo <hambasahaya0303@gmail.com>
2023-02-12 16:57:28 +0700

    renamed:    public/index.html -> index.html 	renamed:    package.json -> public/package.json 	modified:   public/script.js 	renamed:    public/styles.css -> styles.css

--- public/script.js
+++ public/script.js
@@ -1,9 +1,18 @@
 
 const hamburger = document.getElementById("navbar-anchor");
 
-  function Open() {
+let isClicked = true;
+
+
+function Open() {
+  if (isClicked) {
+    hamburger.classList.remove("hidden");
+    isClicked = false;
+  } else {
     hamburger.classList.add("hidden");
+    isClicked = true;
   }
 
+}
 
-  
+
