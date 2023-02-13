dfd44721e4c90ad656ad8968fb4ef6cf0237253e
hansfigo <hambasahaya0303@gmail.com>
2023-02-14 00:18:15 +0700

    fix-navbar

--- public/script.js
+++ public/script.js
@@ -1,12 +1,25 @@
 
 const hamburger = document.getElementById("navbar-anchor");
+const hambIcon = document.getElementById("hamb-ic");
+const crossIcon = document.getElementById("cross-ic");
 
-let isClicked = true;
+let isClicked = false;
 
 
 function Open() {
+
   hamburger.classList.toggle("h-64")
   hamburger.classList.toggle("border-t-2")
+
+  if (!isClicked) {
+    hambIcon.classList.add("hidden")
+    crossIcon.classList.remove("hidden")
+    isClicked = true;
+  } else {
+    hambIcon.classList.remove("hidden")
+    crossIcon.classList.add("hidden")
+    isClicked = false;
+  }
   // if (isClicked) {
   //   hamburger.classList.remove("h-[0px]");
   //   hamburger.classList.add("h-full");
