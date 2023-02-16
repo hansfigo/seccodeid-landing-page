23957cd9ae0c0e29df59c56e4a40a80a71ad3e39
hansfigo <hambasahaya0303@gmail.com>
2023-02-17 03:03:34 +0700

    events-card

--- src/script.js
+++ src/script.js
@@ -53,3 +53,21 @@ class MyElement extends HTMLElement {
   }
 }
 customElements.define('event-card', MyElement);
+
+class ClockIcon extends HTMLElement {
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    const title = this.getAttribute('title');
+    const img = this.getAttribute('img');
+    this.innerHTML = `<svg class="h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+  </svg>
+  `;
+  }
+}
+customElements.define('clock-ic', ClockIcon);
+
+
